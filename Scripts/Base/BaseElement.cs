ac33f653822d57b5bbdcbfcf3aeb9029ec89e8e0
BoBoBlack <1026512434@qq.com>
2021-05-20 09:57:35 +0800

    First Commit

---
+++ Scripts/Base/BaseElement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseElement : BaseMonoBehaviour
+{
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+    }
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+    public override void OnFixedUpdate()
+    {
+        base.OnFixedUpdate();
+    }
+    public override void OnLateUpdate()
+    {
+        base.OnLateUpdate();
+    }
+}
---
+++ Scripts/Base/BaseMonoBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMonoBehaviour : MonoBehaviour
+{
+    private void Awake()
+    {
+        OnAwake();
+    }
+    private void Start()
+    {
+        OnStart();
+    }
+    private void Update()
+    {
+        OnUpdate();
+    }
+    private void FixedUpdate()
+    {
+        OnFixedUpdate();
+    }
+    private void LateUpdate()
+    {
+        OnLateUpdate();
+    }
+    public virtual void OnAwake()
+    {
+
+    }
+    public virtual void OnStart()
+    {
+
+    }
+    public virtual void OnUpdate()
+    {
+
+    }
+    public virtual void OnFixedUpdate()
+    {
+
+    }
+    public virtual void OnLateUpdate()
+    {
+
+    }
+    public virtual void OnReset()
+    {
+
+    }
+}
